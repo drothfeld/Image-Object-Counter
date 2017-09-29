84e08d0462984d09e037393e5fe5bf6646e3576f
Dylan Rothfeld <drothfel@gmail.com>
2017-09-29 10:59:07 -0700

    changed parameter and file upload logic

--- imageObjectCounter.py
+++ imageObjectCounter.py
@@ -3,12 +3,31 @@
 # For OSX use the command: "pip install scikit-image --user"
 
 # Imports
-from skimage import io, filters
+from skimage import io, filters, measure
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import logging
+import sys
 
-im = io.imread('test00.jpg', as_grey=True)
-val = filters.threshold_otsu(im)
-drops = ndimage.binary_fill_holes(im < val)
-plt.imshow(drops, cmap='gray')
-plt.show()
+# Checking Script Arguments
+displayImage = False
+filename = sys.argv[-1]
+if len(sys.argv) == 3:
+    if sys.argv[1] == '-d':
+        displayImage = True
+    # Invalid parameter entered
+    else:
+        logging.error(' | Invalid parameter used: ' + sys.argv[1])
+        quit()
+
+if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+    # Running Image Analysis
+    logging.info(' | Image file successfully read.\n')
+    im = io.imread(filename, as_grey=True)
+    val = filters.threshold_otsu(im)
+    drops = ndimage.binary_fill_holes(im < val)
+    if displayImage:
+        plt.imshow(drops, cmap='gray')
+        plt.show()
+else:
+    logging.error(' | No correct image file was specified of type: .png .jpg .jpeg\n')
